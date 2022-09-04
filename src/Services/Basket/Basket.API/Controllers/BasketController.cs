6c21e4038821f47f563338f97519a353f477bb71
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-09-04 14:48:44 +0300

    Added basket service

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,49 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class BasketController : ControllerBase
+{
+    private readonly IBasketRepository _repository;
+
+    public BasketController(IBasketRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    [HttpGet("{userName}", Name = "GetBasket")]
+    [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+    public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+    {
+        var basket = await _repository.GetBasketAsync(userName);
+        return Ok(basket ?? new ShoppingCart(userName));
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+    public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+    {
+        // TODO : Communicate with Discount.Grpc
+        // and Calculate latest prices of product into shopping cart
+        // consume Discount Grpc
+        // foreach (var item in basket.Items)
+        // {
+        //     var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+        //     item.Price -= coupon.Amount;
+        // }
+
+        return Ok(await _repository.UpdateBasketAsync(basket));
+    }
+    
+    [HttpDelete("{userName}", Name = "DeleteBasket")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    public async Task<IActionResult> DeleteBasket(string userName)
+    {
+        await _repository.DeleteBasketAsync(userName);
+        return Ok();
+    }
+}
