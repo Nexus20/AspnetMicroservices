7f255cb06e062c6b0a22b734be8c8dde9d2c9510
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-09-09 19:04:42 +0300

    Implemented GRPC service

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,5 @@
 using Discount.Grpc.Extensions;
+using Discount.Grpc.Mapper;
 using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
@@ -9,14 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddAutoMapper(typeof(DiscountProfile));
 
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 app.MigrateDatabase<Program>();
-
+app.MapGrpcService<DiscountService>();
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
