e77e1414a886281f04b2ac2bcfc5edc450640c8e
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2022-10-02 16:45:41 +0300

    Ordering microservice

---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Models;
+
+public class EmailSettings {
+    public string ApiKey { get; set; } = null!;
+    public string FromAddress { get; set; } = null!;
+    public string FromName { get; set; } = null!;
+}
