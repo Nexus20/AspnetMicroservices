94fcc3f906eaaf34b7ec9621b350b204f7181ee3
Yevhen Chubarov <40770350+Nexus20@users.noreply.github.com>
2023-02-23 16:51:26 +0200

    Added aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,33 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public class CatalogService : ICatalogService
+{
+    private readonly HttpClient _httpClient;
+
+
+    public CatalogService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IEnumerable<CatalogModel>> GetCatalogAsync()
+    {
+        var response = await _httpClient.GetAsync("/api/v1/Catalog");
+        return await response.ReadContentAs<List<CatalogModel>>();
+    }
+
+    public async Task<IEnumerable<CatalogModel>> GetCatalogByCategoryAsync(string category)
+    {
+        var response = await _httpClient.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}");
+        return await response.ReadContentAs<List<CatalogModel>>();
+    }
+
+    public async Task<CatalogModel> GetCatalogByIdAsync(string id)
+    {
+        var response = await _httpClient.GetAsync($"/api/v1/Catalog/{id}");
+        return await response.ReadContentAs<CatalogModel>();
+    }
+}
---
+++ src/ApiGateways/Shopping.Aggregator/Services/ICatalogService.cs
@@ -0,0 +1,10 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+
+public interface ICatalogService
+{
+    Task<IEnumerable<CatalogModel>> GetCatalogAsync();
+    Task<IEnumerable<CatalogModel>> GetCatalogByCategoryAsync(string category);
+    Task<CatalogModel> GetCatalogByIdAsync(string id);
+}
